b156776c6dc01b8f19068b5b8870e38d48ab32f3
Pablo Martín <goinnn@gmail.com>
2012-10-23 11:40:22 +0200

    jslit parser

--- ajax_validation/static/ajax_validation/js/jquery-ajax-validation.js
+++ ajax_validation/static/ajax_validation/js/jquery-ajax-validation.js
@@ -1,53 +1,58 @@
-(function($) {
-    function inputs(form)   {
+(function ($) {
+    "use strict";
+    function inputs(form) {
         return form.find(":input:visible:not(:button)");
     }
 
-    var errorRemover = {
-    p: function(form, fields){
-        get_form_error_position(form, '__all__').prev('ul.errorlist').remove();
-        if (!fields)
-            form.find('ul.errorlist').remove();
-        else
-            fields.closest('p').prev('ul.errorlist').remove();
-    },
-    table: function(form, fields){
-        form.find('tr:has(ul.errorlist):not(:has(label))').remove();
-        if (!fields)
-            inputs(form).prev('ul.errorlist').remove();
-        else
-            fields.prev('ul.errorlist').remove();
-
-    },
-    ul: function(form, fields){
-        form.find('li:has(ul.errorlist):not(:has(label))').remove();
-        if (!fields)
-            inputs(form).prev().prev('ul.errorlist').remove();
-        else
-            fields.prev().prev('ul.errorlist').remove();
-    },
-    };
-
-    function removeErrors(form, form_type, fields){
-        if (fields)
-            fields = $(fields.map(function(n){ return '*[name="'+n+'"]'; }).join(', '));
-       return errorRemover[form_type](form, fields);
-    }
-
-
     function get_form_error_position(form, key) {
         key = key || '__all__';
-        if (key == '__all__') {
-            var filter = ':first';
+        var filter;
+        if (key === '__all__') {
+            filter = ':first';
         } else {
-            var filter = ':first[id^=id_' + key.replace('__all__', '') + ']';
+            filter = ':first[id^=id_' + key.replace('__all__', '') + ']';
         }
         return inputs(form).filter(filter).parent();
+    }
+
+    var errorRemover = {
+        p: function (form, fields) {
+            get_form_error_position(form, '__all__').prev('ul.errorlist').remove();
+            if (!fields) {
+                form.find('ul.errorlist').remove();
+            } else {
+                fields.closest('p').prev('ul.errorlist').remove();
+            }
+        },
+        table: function (form, fields) {
+            form.find('tr:has(ul.errorlist):not(:has(label))').remove();
+            if (!fields) {
+                inputs(form).prev('ul.errorlist').remove();
+            } else {
+                fields.prev('ul.errorlist').remove();
+            }
+
+        },
+        ul: function (form, fields) {
+            form.find('li:has(ul.errorlist):not(:has(label))').remove();
+            if (!fields) {
+                inputs(form).prev().prev('ul.errorlist').remove();
+            } else {
+                fields.prev().prev('ul.errorlist').remove();
+            }
+        }
     };
 
+    function removeErrors(form, form_type, fields) {
+        if (fields) {
+            fields = $(fields.map(function (n) {return '*[name="' + n + '"]'; }).join(', '));
+        }
+        return errorRemover[form_type](form, fields);
+    }
+
     var errorInjector = {
-    p: function(form, data) {
-            $.each(data.errors, function(key, val) {
+        p: function (form, data) {
+            $.each(data.errors, function (key, val) {
                 if (key.indexOf('__all__') >= 0) {
                     var error = get_form_error_position(form, key);
                     if (error.prev().is('ul.errorlist')) {
@@ -60,8 +65,8 @@
                 }
             });
         },
-    table: function(form, data) {
-            $.each(data.errors, function(key, val) {
+        table: function (form, data) {
+            $.each(data.errors, function (key, val) {
                 if (key.indexOf('__all__') >= 0) {
                     get_form_error_position(form, key).parent().before('<tr><td colspan="2"><ul class="errorlist"><li>' + val + '</li></ul></td></tr>');
                 } else {
@@ -69,19 +74,19 @@
                 }
             });
         },
-    ul: function(form, data)  {
-            $.each(data.errors, function(key, val) {
+        ul: function (form, data) {
+            $.each(data.errors, function (key, val) {
                 if (key.indexOf('__all__') >= 0) {
                     get_form_error_position(form, key).before('<li><ul class="errorlist"><li>' + val + '</li></ul></li>');
                 } else {
                     $('#' + key, form).prev().before('<ul class="errorlist"><li>' + val + '</li></ul>');
                 }
             });
-        },
+        }
     };
 
-    function injectErrors(type, form, data){
-    errorInjector[type](form, data);
+    function injectErrors(type, form, data) {
+        errorInjector[type](form, data);
     }
 
     function getUntilCurrent(ev, nodeNames, form) {
@@ -89,13 +94,13 @@
         if (current.tagName.toLowerCase() === "option") {
             current = $(current).parents("select")[0];
         }
-         if (nodeNames.split(", ").indexOf(current.nodeName.toLowerCase()) === -1) {
+        if (nodeNames.split(", ").indexOf(current.nodeName.toLowerCase()) === -1) {
             return false;
         }
         return current;
     }
 
-    $.fn.validate = function(url, settings) {
+    $.fn.validate = function (url, settings) {
         settings = $.extend({
             type: 'table',
             callback: false,
@@ -108,10 +113,10 @@
             getUntilCurrent: getUntilCurrent
         }, settings);
 
-        return this.each(function() {
+        return this.each(function () {
             var form = $(this);
             var current = null;
-            var validation = function(ev, is_submitting)  {
+            var validation = function (ev, is_submitting) {
                 var responseData = {};
                 var data = form.serialize();
                 var nodeNames = "input, select, textarea";
@@ -124,11 +129,11 @@
                     }
                     var f = $(current).parents("form");
                     var find = false;
-                    var fieldsName = []
+                    var fieldsName = [];
                     var fields = f.find(nodeNames).each(function () {
                         if (!find) {
                             fieldsName[fieldsName.length] = $(this).attr("name");
-                        } 
+                        }
                         if (this === current) {
                             find = true;
                             return;
@@ -141,21 +146,22 @@
                     data: data,
                     dataType: 'json',
                     traditional: true,
-                    error: function(XHR, textStatus, errorThrown)   {
+                    error: function (XHR, textStatus, errorThrown) {
                     },
-                    success: function(data, textStatus) {
+                    success: function (data, textStatus) {
                         responseData = data;
                         var is_valid = data.valid;
                         var fields = settings.fields;
                         var fieldsTags = form.find(nodeNames);
                         var fieldsToRemove = [];
+                        var i;
                         if (current !== null) {
-                            for (var i=0; i<fieldsTags.length; i++) {
+                            for (i = 0; i < fieldsTags.length; i = i + 1) {
                                 var field = fieldsTags[i];
-                                if (fields !== null || fields.indexOf(f) != -1) {
+                                if (fields !== null || fields.indexOf(field) !== -1) {
                                     fieldsToRemove[fieldsToRemove.length] = field.name;
                                 }
-                                if (field == current) {
+                                if (field === current) {
                                     break;
                                 }
                             }
@@ -179,9 +185,9 @@
                     type: 'POST',
                     url: url
                 });
-            }
+            };
             settings.dom.bind(settings.event, validation);
-            if (settings.event != "submit") {
+            if (settings.event !== "submit") {
                 settings.dom.find("input[type=submit]").click(function (ev) {
                     ev.preventDefault();
                     validation(ev, true);
